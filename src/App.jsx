94e8686b72930585d143b3be81e075e32e1c31ce
Ema <emaarias_nob@hotmail.com>
2026-04-05 13:22:25 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,21 @@ import stockReal from './data/stock_final.json'
 
 // Diccionario para transformar abreviaturas en nombres legibles
 const SET_NAMES = {
-  "PAF": "Paldean Fates",
-  "MEG": "Mega Evolution",
-  "PFL": "Phantasmal Flames",
-  "JTG": "Journey Together",
-  "ASC": "Ascended Heroes",
-  "SSP": "Surging Sparks",
-  "SCR": "Stellar Crown",
-  "PAR": "Paradox Rift",
-  "TWM": "Twilight Masquerade",
-  "OBF": "Obsidian Flames",
-  "PAL": "Paldea Evolved",
-  "DRI": "Destined Rivals",
-  "TEF": "Temporal Forces",
-  "SVI": "Scarlet & Violet Base",
-  "F": "Silver Tempest"
+  "PAF": "PAF - Paldean Fates",
+  "MEG": "MEG - Mega Evolution",
+  "PFL": "PFL - Phantasmal Flames",
+  "JTG": "JTG - Journey Together",
+  "ASC": "ASC - Ascended Heroes",
+  "SSP": "SSP - Surging Sparks",
+  "SCR": "SCR - Stellar Crown",
+  "PAR": "PAR - Paradox Rift",
+  "TWM": "TWM - Twilight Masquerade",
+  "OBF": "OBF - Obsidian Flames",
+  "PAL": "PAL - Paldea Evolved",
+  "DRI": "DRI - Destined Rivals",
+  "TEF": "TEF - Temporal Forces",
+  "SVI": "SVI - Scarlet & Violet Base",
+  "F": "F - Silver Tempest"
 };
 
 function App() {
